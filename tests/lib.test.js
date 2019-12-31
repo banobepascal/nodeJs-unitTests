b87db8cb3bcbd844e255d35257827d76dd4d2097
banobepascal <banobepascal@gmail.com>
2019-12-31 12:36:26 +0300

    ft(first unit test):
    - wrote first unit test
    - grouped lib.test.js test
    - refactored function to test
    
    Finishes

---
+++ tests/lib.test.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+const lib = require('../testing-demo/lib');
+
+describe('absolute', () => {
+    it('should return a positive number if input is positive', () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return positive number if input is negative', () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return 0 if input is 0', () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
+});
+
