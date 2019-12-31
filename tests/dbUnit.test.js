9a1f862f0ec26e1fe33ec013468171ff6d7a3caa
banobepascal <banobepascal@gmail.com>
2019-12-31 17:33:50 +0300

    ft(finishing tests)
    - added testing with mock functions
    - added more tests with jest inbuilt mock functions
    
    Finishes

---
+++ tests/dbUnit.test.js
@@ -0,0 +1,34 @@
+const lib = require('../testing-demo/lib');
+const db = require('../testing-demo/db');
+const mail = require('../testing-demo/mail');
+
+describe('applyDiscount', () => {
+    it('should apply 10% discount if customer has more than 10 points', () => {
+        db.getCustomerSync = (customerId) => {
+            console.log('Fake reading ....');
+            return { id: customerId, points: 20 };
+        }
+
+        const order = { customerId: 1, totalPrice: 10};
+        lib.applyDiscount(order);
+        expect(order.totalPrice).toBe(9);
+    });
+});
+
+describe('notifyCustomer', () => {
+    it('should send an email to the customer', () => {
+        db.getCustomerSync = jest.fn().mockReturnValue({ email: 'a' });
+        mail.send = jest.fn();
+        // db.getCustomerSync = function(customerId) {
+        //     return { email: 'a' };
+        // }
+        // let mailSent = false;
+        // mail.send = (email, message) => {
+        //     mailSent = true;
+        // }
+        lib.notifyCustomer({ customerId: 1 });
+        // expect(mailSent).toBe(true);
+        expect(mail.send.mock.calls[0][1]).toMatch(/order/);
+        expect(mail.send).toHaveBeenCalled();
+    });
+});
---
+++ tests/exercise1.test.js
@@ -0,0 +1,30 @@
+const lib = require('../testing-demo/exercise1');
+
+describe('fizzBuzz', () => {
+    it('should throw exception if input is not number', () => {
+        const args = ['a', null, undefined, false];
+        args.forEach(a => {
+            expect(() => { lib.fizzBuzz(a) }).toThrow();
+        })
+    });
+
+    it('should return FizzBuzz if number is a multiple of 3 & 5', () => {
+        const result = lib.fizzBuzz(15);
+        expect(result).toBe('FizzBuzz');
+    });
+
+    it('should return Fizz if number is a multiple of 3', () => {
+        const result = lib.fizzBuzz(6);
+        expect(result).toBe('Fizz');
+    });
+
+    it('should return Buzz if number is a multiple of 5', () => {
+        const result = lib.fizzBuzz(10);
+        expect(result).toBe('Buzz');
+    });
+
+    it('should return input if number is not a multiple of 3 & 5', () => {
+        const result = lib.fizzBuzz(1);
+        expect(result).toBe(1);
+    });
+});
